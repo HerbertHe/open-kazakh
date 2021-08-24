7e333d6aa4dcfa1ac6c4c8c2f7f570413944ddcd
Herbert <herbert.he0229@gmail.com>
2021-08-24 22:25:25 +0800

    fix :bug: : 修复响应式适配

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Header = () => (
-    <div className="flex flex-row bg-[rgba(255,255,255,0.9)] fixed top-0 left-0 right-0 w-full border p-4 px-20 select-none z-9999">
+    <div className="flex flex-row bg-[rgba(255,255,255,0.9)] fixed top-0 left-0 right-0 w-full border p-4 px-20 select-none z-9999 <sm:(p-4)">
         <h1 className="text-xl">Open Kazakh</h1>
     </div>
 )
